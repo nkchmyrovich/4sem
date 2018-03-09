5161b5ac83e03a98bd896cb407ba170d1ffefe8f
Vlad <vlad.miryaha@gmail.com>
2018-03-09 21:06:08 +0300

    ok

--- PopularWords/text.cpp
+++ PopularWords/text.cpp
@@ -24,33 +24,18 @@ int main(int argc, char const *argv[]) {
 	std::ifstream file("_file.txt");
 	if (!file.is_open()) {
 		std::cout << "ERROR: input file cannot be opened\n";
-		// кажется, что можно не делать close, если не получилось открыть файл
-		//Ok
 		return 0;
 	}
 	std::string word;
 	std::map<std::string, int> myMap;
 	while (!file.eof()) {
-		file >> word; // только эта строка бросает исключения
-		/// судя по http://www.cplusplus.com/reference/istream/istream/operator%3E%3E/ вы ловите не единственный вариант
-		/// мне кажется, что для 1го упражнения в семестре можно не заморачиваться с исключениями здесь и убрать try/catch
-		//Ок
-
-		//Просто мне очень часто компилятор кидал это исключение std::length_error
-		//Так что я на всякий случай сделал try/catch
-
-		std::cout << word << "\n";
+		file >> word;
 		prepare(word);
-		if (word.size() == 0) continue;
-		myMap[word]++;
-
-		// зачем делать clear?
-		//Ок, убрал
+		if (word.size() > 0) 
+			myMap[word]++;
 	}
 	std::vector<Statistics> s;
 	Statistics elem;
-	// лучше const auto& it, чтобы подчеркнуть, что мы не намерены изменять myMap
-	//Ок
 	for (const auto& it : myMap) {
 		elem.word = it.first;
 		elem.count = it.second;
@@ -59,8 +44,6 @@ int main(int argc, char const *argv[]) {
 	std::sort(s.begin(), s.end(), comparator);
 	std::ofstream file_output("output.txt");
 	if (!file_output.is_open()) {
-		// ? вроде не нужно
-		//Ок
 		std::cout << "ERROR: output file cannot be opened\n";
 		return 0;
 	}
