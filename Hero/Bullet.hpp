9fed95d761dd365b994a98669e9d07a0447f8f28
Vlad <vlad.miryaha@gmail.com>
2018-04-14 01:54:25 +0300

    fixit

--- Hero/Bullet.hpp
+++ Hero/Bullet.hpp
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Хмурович Никита Андреевич. All rights reserved.
 //
 
+// fixit: используйте #pragma once вместо guard'ов типа ifndef 
+
 #ifndef Bullet_hpp
 #define Bullet_hpp
 
@@ -13,9 +15,11 @@
 #include <SFML/Graphics.hpp>
 
 class Bullet {
+    // fixit: зачем каждой пуле знать какое-то время ... оно же общее для все пуль ... если у переменной time другое значение, то назовите яснее переменную и добавьте комментарий
     float time_;
     float drawTime_;
     sf::Vector2f speed_;
+    // нездорово смешивать отрисовку и логику в одном коде ... но для этого упражнения пусть останется так
     sf::CircleShape bullet_;
     sf::Vector2f position_;
 public:
@@ -27,6 +31,8 @@ public:
     void SetDrawTime (float time);
     void SetSpeed (sf::Vector2f& speed);
     bool IsInBounds ();
+    
+    // fixit: зачем нам здесь отдельно объявлять пустой конструктор
     Bullet () = default;
 };
 
